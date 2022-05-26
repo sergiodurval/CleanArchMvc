f1b98715bd3db38afe8e92b8eb8da529d6a79397
sergio durval <sergiodurval2@gmail.com>
2022-05-26 19:03:55 -0300

    Implementando a camada de cross-cutting e criando as migrations

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Migrations/20220526215107_SeedProducts.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) values('Caderno espiral','Caderno espiral 100 folhas','7.45',50,'caderno1.jpg',1)");
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) values('Estojo escolar','Estojo escolar cinza','5.60',70,'estojo1.jpg',1)");
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) values('Borracha escolar','Borracha branca pequena','3.25',80,'borracha01.jpg',1)");
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) values('Calculadora escolar','Calculadora simples','15.39',20,'calculadora.jpg',2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
