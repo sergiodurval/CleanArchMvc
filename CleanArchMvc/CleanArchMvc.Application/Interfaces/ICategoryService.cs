331be595c0b1e1607e7862d9d31670c9580e5ce2
sergio durval <sergiodurval2@gmail.com>
2022-05-27 16:51:36 -0300

    implementando o serviço para categorias

---
+++ CleanArchMvc/CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMvc.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? Id);
+        Task Add(CategoryDTO categoryDTO);
+        Task Update(CategoryDTO categoryDTO);
+        Task Remove(int? Id);
+
+    }
+}
