cb84b1d83b0d2e0846bddfbadbd1e2a3865bc80c
sergio durval <sergiodurval2@gmail.com>
2022-06-03 18:42:09 -0300

    controller para realizar a autenticacao e autorizacao

---
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public AccountController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
