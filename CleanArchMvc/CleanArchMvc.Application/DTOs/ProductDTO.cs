c1cef28376cabb95aa236e44568cfebb53ffb466
sergio durval <sergiodurval2@gmail.com>
2022-06-06 16:04:08 -0300

    implementacao da controller de produtos

--- CleanArchMvc/CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc/CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.Application.DTOs
@@ -42,6 +43,7 @@ namespace CleanArchMvc.Application.DTOs
         [DisplayName("Product Image")]
         public string Image { get; set; }
 
+        [JsonIgnore]
         public Category Category { get; set; }
 
         [DisplayName("Categories")]
