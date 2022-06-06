3d8a354019e419d25a9378a8f58f451cfc8a9c05
sergio durval <sergiodurval2@gmail.com>
2022-06-06 11:38:07 -0300

    incluindo o projeto api

---
+++ CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if(categories == null)
+            {
+                return NotFound("Categories not found");
+            }
+
+            return Ok(categories);
+        }
+    }
+}
