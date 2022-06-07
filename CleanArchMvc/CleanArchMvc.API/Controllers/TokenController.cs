d53c5f860b51239cbd281b84bc7813aa81ae6632
sergio durval <sergiodurval2@gmail.com>
2022-06-07 15:35:07 -0300

    implementando a autenticacao na api

---
+++ CleanArchMvc/CleanArchMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,75 @@
+﻿using CleanArchMvc.API.Models;
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authenticate;
+        private readonly IConfiguration _configuration;
+        public TokenController(IAuthenticate authenticate, IConfiguration configuration)
+        {
+            _authenticate = authenticate;
+            _configuration = configuration;
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody]LoginModel userInfo)
+        {
+            var result = await _authenticate.Authenticate(userInfo.Email, userInfo.Password);
+
+            if(result)
+            {
+                return GenerateToken(userInfo);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+
+        private UserToken GenerateToken(LoginModel userInfo)
+        {
+            var claims = new[]
+            {
+                new Claim("email",userInfo.Email),
+                new Claim("meuvalor","teste"),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var privateKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            //gerando o token
+            JwtSecurityToken token = new JwtSecurityToken
+            (
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credentials
+             );
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+               
+        }
+    }
+}
